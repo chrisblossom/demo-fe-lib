82ec020a853644219936030df50d9c2a2d36f75e
Chris Blossom <cbloss@gmail.com>
2020-05-11 11:52:04 -0700

    initial commit

---
+++ __sandbox__/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import FeLib from './fe-dist/fe-lib'
+
+function App() {
+  return (
+    <div className="App">
+		<FeLib/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/fe-lib.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+export default () => (
+	<h1>
+		This is your library
+	</h1>
+);
